eb73af4bf9eb9db17f5a850c22a487340fa853de
flyforfun98 <48056951+flyforfun98@users.noreply.github.com>
2019-12-20 10:25:44 +0200

    Added Chapters from 1 to 9

---
+++ shop/src/main/java/ro/msg/learning/shop/ShopApplication.java
@@ -0,0 +1,30 @@
+package ro.msg.learning.shop;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ro.msg.learning.shop.repository.RepositoryFactory;
+import ro.msg.learning.shop.seed.MainSeed;
+
+@SpringBootApplication
+@RequiredArgsConstructor
+@EnableScheduling
+public class ShopApplication {
+
+	private final RepositoryFactory repositoryFactory;
+	private final PasswordEncoder passwordEncoder;
+
+	@Bean
+	public CommandLineRunner initializeDatabase() {
+		return new MainSeed(repositoryFactory, passwordEncoder);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopApplication.class, args);
+	}
+
+}
